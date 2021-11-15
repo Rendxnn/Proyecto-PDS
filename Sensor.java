fcce9be515882fdff8a98e5ea4078e1d37c2e9c9
Rendxnn <88605945+Rendxnn@users.noreply.github.com>
2021-11-15 18:27:07 -0500

    Update Sensor.java

--- Sensor.java
+++ Sensor.java
@@ -1,11 +1,7 @@
 public class Sensor {
 
     public boolean entrada(int senal, int senal2) {
-        if (senal > senal2) {
-            return true;
-        } else {
-            return false;
-        }
+        return senal > senal2;
     }
 
     public boolean salida(int senal, int senal2) {
